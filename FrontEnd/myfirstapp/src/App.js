bc9c373def876ce77087020759986ed023b4af4e
Devin Amalean s3821117 <s3821117@student.rmit.edu.au>
2021-08-21 02:27:23 +1000

    Added HeaderAdmin to the structure

--- FrontEnd/myfirstapp/src/App.js
+++ FrontEnd/myfirstapp/src/App.js
@@ -19,6 +19,7 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <div className="App">
+            {/* <HeaderAdmin /> */}
             <Header />
             {
               //Public Routes
