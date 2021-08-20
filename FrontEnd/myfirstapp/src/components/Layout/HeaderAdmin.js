c1a382dc30f97bf1a562486bcb7fc4f82c8e00fd
Devin Amalean s3821117 <s3821117@student.rmit.edu.au>
2021-08-21 01:55:15 +1000

    Created a header for when the admin is logged in and added the 'Add Book' button

---
+++ FrontEnd/myfirstapp/src/components/Layout/HeaderAdmin.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+class HeaderAdmin extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
+                <div className="container">
+                    {/* Left  of the Nvaigation Bar */}
+                    <ul className="nav navbar-nav pull-sm-left">
+                        <li className="nav-item">
+                            <a className="navbar-brand" href="#">
+                                <img src="/images/DSC_5703.jpg" width="30" height="30"></img>
+                            </a>
+                        </li>
+                    </ul>
+
+                    {/* Centre of the navigation bar */}
+                    <ul className="nav navbar-nav navbar-logo mx-auto">
+                        <li className="nav-item">
+                            <a className="navbar-brand adminNavBar" href="Dashboard.html">
+                                Bookeroo
+                            </a>
+                        </li>
+                    </ul>
+
+                    {/* Right of the navigagtion bar */}
+                    <ul className="nav navbar-nav pull-sm-right">
+                        <li className="nav-item">
+                            <button className="btn btn-light my-2 my-sm-0 addBookButton">Add Book</button>
+                        </li>
+                        <li className="nav-item">
+                            <button className="btn btn-light my-2 my-sm-0 logOutButton">Log Out</button>
+                        </li>
+                    </ul>
+
+                
+                </div>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default HeaderAdmin;
