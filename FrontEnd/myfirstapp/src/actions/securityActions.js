8cfc52697d9d6df63728b4566a411591d75abaec
s3850825 <s3850825@student.rmit.edu.au>
2021-08-31 13:48:10 +1000

    Fixed the issue that sgin up page must redirect to login page after click submit button by adding type:SET_CURRENT_USER

--- FrontEnd/myfirstapp/src/actions/securityActions.js
+++ FrontEnd/myfirstapp/src/actions/securityActions.js
@@ -9,6 +9,7 @@ export const createNewUser = (newUser, history) => async dispatch => {
         await axios.post("http://localhost:8080/api/users/register", newUser);
         history.push("/login");
         dispatch({
+            type: GET_ERRORS,
             payload: {}
         });
     }
