0652c3309a66bc3d21d8d3f0c36f306705c4cbf6
Jainam Doshi <s3825891@student.rmit.edu.au>
2021-09-18 08:57:06 +0400

    Fix home page css

--- FrontEnd/myfirstapp/src/components/Books/ShowAllBooks.js
+++ FrontEnd/myfirstapp/src/components/Books/ShowAllBooks.js
@@ -41,13 +41,13 @@ class ShowAllBooks extends Component {
                     </form>
                 </div>
                 <br />
-                <div className="row">
+                <div className="allBooks">
                     {this.state.allBooks.map(book => (
-                    <div className="col-md-2" align="center">
+                    <div key={book} className="oneBook">
                         <Link to = {`/book/${book.isbn}`}>
-                        <img src={book.bookCoverURL} alt={`${book.isbn}`} />
-                        <h4>{book.bookName}</h4>
-                        <h4>{book.author}</h4>
+                            <img className="bookImage" src={book.bookCoverURL} alt={`${book.isbn}`} />
+                            <h4>{book.bookName}</h4>
+                            <h4>{book.author}</h4>
                         </Link>
                     </div>
                         ))}
