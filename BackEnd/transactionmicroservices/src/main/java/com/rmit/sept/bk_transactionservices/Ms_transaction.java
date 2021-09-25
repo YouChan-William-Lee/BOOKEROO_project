98a08c836c2f817f3ec6f3df1f22307545b0ce41
s3850825 <s3850825@student.rmit.edu.au>
2021-09-25 17:33:29 +1000

    new branch transaction has been created

---
+++ BackEnd/transactionmicroservices/src/main/java/com/rmit/sept/bk_transactionservices/Ms_transaction.java
@@ -0,0 +1,13 @@
+package com.rmit.sept.bk_transactionservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms_transaction {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ms_transaction.class, args);
+    }
+
+}
---
+++ BackEnd/transactionmicroservices/src/test/java/com/rmit/sept/bk_transactionservices/MstransactionTests.java
@@ -0,0 +1,13 @@
+package com.rmit.sept.bk_transactionservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MstransactionTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
