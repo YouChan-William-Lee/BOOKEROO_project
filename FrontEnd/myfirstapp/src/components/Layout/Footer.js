2fde1479c6e99a05cd6a2b6fd34282c917e8839b
s3821245 <s3821245@student.rmit.edu.au>
2021-10-05 01:32:23 +0530

    added margin to footer

--- FrontEnd/myfirstapp/src/components/Layout/Footer.js
+++ FrontEnd/myfirstapp/src/components/Layout/Footer.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 class Footer extends Component {
     render() {
         return (
-            <div className="theFooter ">
+            <div className="theFooter mt-3">
                 <nav className="navbar fixed-bottom navbar-expand-sm navbar-dark bg-primary mb-0 mt-4">
                     <div className="container">
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
@@ -21,7 +21,7 @@ class Footer extends Component {
 
                             <ul className="navbar-nav ml-auto">
                                 <li className="nav-item">
-                                    <Link to="contactus"><a className="nav-link " href="register"> CONTACT US </a></Link>
+                                    <Link to="/contactus"><a className="nav-link " href="register"> CONTACT US </a></Link>
                                 </li>
                             </ul>
                         </div>
