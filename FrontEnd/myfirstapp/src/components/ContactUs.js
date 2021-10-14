32c350219829ec9e221f42fc4014748312603ef0
Devin <devinamalean25@gmail.com>
2021-10-15 05:03:04 +1100

    Updated profile pictures for contact us page

--- FrontEnd/myfirstapp/src/components/ContactUs.js
+++ FrontEnd/myfirstapp/src/components/ContactUs.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import "../Stylesheets/ContactUs.css"
-import profilePic1 from '../Images/DSC_5703.jpg'
+import profilePic1 from '../Images/dev.jpg'
+import profilePic2 from '../Images/jamDos.jpeg'
 import profilePicDummy from '../Images/profileImage.png'
 
 class ContactUs extends Component {
@@ -35,7 +36,7 @@ class ContactUs extends Component {
 
                             <div className="col-md-3">
                                 <div className="card" style={{width: '19rem'}}>
-                                    <img class="card-img-top profilePic" src={profilePicDummy} alt="Card image cap"/>
+                                    <img class="card-img-top profilePic" src={profilePic2} alt="Card image cap"/>
                                     <div className="card-body">
                                         <p className="card-text display-4">Jainam Doshi</p>
                                         <p className="card-text lead">Scrum Master</p>
